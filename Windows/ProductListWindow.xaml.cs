431bff4c7f9edc8d991909cd692909b5f42b8708
Forel0ck <tysim190@gmail.com>
2023-02-20 17:04:32 +0300

    2

---
+++ Windows/ProductListWindow.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using _163OnMyNeckFeatOgBurmikFtAnisimov.BD;
+using _163OnMyNeckFeatOgBurmikFtAnisimov.ClassHelper;
+using static _163OnMyNeckFeatOgBurmikFtAnisimov.ClassHelper.EFClass;
+
+
+namespace _163OnMyNeckFeatOgBurmikFtAnisimov.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductListWindow.xaml
+    /// </summary>
+    public partial class ProductListWindow : Window
+    {
+        public ProductListWindow()
+        {
+            InitializeComponent();
+            getAA();
+        }
+        private void getAA()
+        {
+            List<Product> prodLists = new List<Product>();
+            prodLists = context.Product.ToList();
+            ProductList.ItemsSource = prodLists;
+        }
+    }
+}
